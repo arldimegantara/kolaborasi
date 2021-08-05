b7ab1ed5804bb1d18c4cfcbf9de8333f1b2e41a5
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-08-05 14:29:34 +0700

    kolaborasi-afif

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "<h2>Selamat berlatih menjadi kolaborator.</h2>";
 echo "<h3>Isi Nama dan Nim anda di bawah baris code ini</h3>";
-echo "195411150 - Muhamad Arldi Megantara";
+echo "195411020 - Muhamad Afif Alfiano Hermasyah";
 ?>
